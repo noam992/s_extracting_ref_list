3608c48f3903f39cbff94ab504aa19a61e28b969
Noam Konja <noam.konja@gmail.com>
2023-08-10 12:15:50 +0300

    improving script and adding chromedriver.exe file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
-from bs4 import *
-import requests
-import json
 import boto3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
 
 # Define env parameters / values
 # first hard coded. later move to env parameter in AWS configuration
@@ -16,40 +20,112 @@ def sanding_sqs_massage(message):
     data['ref'] = message
     print(data)
 
-    sqs_resource = boto3.resource('sqs')
-
-    response = sqs_resource.send_message(
-        QueueUrl=sqs_arn,
-        MessageBody=json.dumps(data)
-    )
-    print(response)
+    # sqs_resource = boto3.resource('sqs')
+    #
+    # response = sqs_resource.send_message(
+    #     QueueUrl=sqs_arn,
+    #     MessageBody=json.dumps(data)
+    # )
+    # print(response)
 
 
 # extract relevant content from HTML
 def lambda_handler(event, context):
-
     try:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+
         for site in site_list:
-            # content of URL
-            r = requests.get(site)
 
-            # Parse HTML Code
-            soup = BeautifulSoup(r.text, 'html.parser')
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.get(site)
 
-            # find all href from 'link' and 'a' elemnts
-            ref_list = soup.find_all('link')
-            ref_list += soup.find_all('a')
+            ref_list = []  # REF LIST for specific site
 
-            for ref in ref_list:
-                if ref.has_attr('href'):  # Check if the tag has an 'href' attribute
-                    href = ref['href']
-                    if not href.endswith(('.css', '.png', '/')):  # Skip if the link ends with .css or .png or /
+            pause = 2  # Increased pause time to allow content to load
 
-                        # Appropriate conditions for specific sites
-                        if (site.__contains__('www.jamesallen.com')) & (not href.startswith(('https://www.jamesallen.com'))):
-                            href = 'https://www.jamesallen.com/' + href
-                            sanding_sqs_massage(href)  # sending url using sqs
-                        else:
-                            sanding_sqs_massage(href)  # sending url using sqs
-    except:
-        pass
+            # Explicitly wait for some element to appear (you can adjust this condition based on your use case)
+            wait = WebDriverWait(driver, 30)  # Wait up to 30 seconds
+            wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+
+            lastHeight = driver.execute_script("return document.body.scrollHeight")
+            # print(lastHeight)
+
+            while True:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(pause)
+                newHeight = driver.execute_script("return document.body.scrollHeight")
+                if newHeight == lastHeight:
+                    break
+                lastHeight = newHeight
+                print(lastHeight)
+
+            # print(driver.page_source) # print the html with the exist HTML tags before effect on div tags (like click on div to open)
+
+            # # ------------------ # # relevant only for https://www.jamesallen.com/loose-diamonds/oval-cut/ website
+            j = 1
+            while j <= 20:
+                i = 0
+                while i <= 22:
+
+                    xpathDiv = f"// *[ @ id = \"_{j}_{i}\"]"
+                    xpathSubDiv = f"// *[ @ id = \"_{j}_{i}\"] / div"
+                    xpathSubSubDiv = f"// *[ @ id = \"_{j}_{i}\"] / div / div[2]"
+                    xpathLink = f"// *[ @ id = \"_{j}_{i}\"] / div / div[2] / a"
+
+                    # print("Href Element: ", f"_{j}_{i}") # Print element number
+
+                    try:
+                        # Main div
+                        # Wait for the element to be clickable
+                        element = WebDriverWait(driver, 10).until(
+                            EC.element_to_be_clickable((By.XPATH, xpathDiv))
+                        )
+                        # Scroll to element and click
+                        driver.execute_script("arguments[0].scrollIntoView();", element)
+                        driver.execute_script("arguments[0].click();", element)
+
+                        # Subdiv
+                        subElement = WebDriverWait(driver, 10).until(
+                            EC.element_to_be_clickable((By.XPATH, xpathSubDiv))
+                        )
+                        # Scroll to element and click
+                        driver.execute_script("arguments[0].scrollIntoView();", subElement)
+                        driver.execute_script("arguments[0].click();", subElement)
+
+                        # SubSubdiv
+                        subSubElement = WebDriverWait(driver, 20).until(
+                            EC.element_to_be_clickable((By.XPATH, xpathSubSubDiv))
+                        )
+                        # Scroll to element and click
+                        driver.execute_script("arguments[0].scrollIntoView();", subSubElement)
+                        driver.execute_script("arguments[0].click();", subSubElement)
+
+                        # Link
+                        linkElement = WebDriverWait(driver, 20).until(
+                            EC.element_to_be_clickable((By.XPATH, xpathLink))
+                        )
+                        # Scroll to element and click
+                        driver.execute_script("arguments[0].scrollIntoView();", linkElement)
+
+                        # Get the value of the 'href' attribute and print it
+                        href_value = linkElement.get_attribute("href")
+                        ref_list.append(href_value)
+                        # print("Href attribute:", href_value)
+
+                    except Exception as e:
+                        # Handle the stale element reference exception if needed
+                        print("An error occurred:", str(e))
+
+                    i += 1
+
+                j += 1
+            # -------------------
+            driver.quit()
+
+            for href in ref_list:
+                print(href)
+                # sanding_sqs_massage(href)  # sending url using sqs
+
+    except Exception as e:
+        print("An error occurred:", str(e))
