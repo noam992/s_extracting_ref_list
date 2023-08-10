3a903e3c6894aae8d6d00c81a81dcc61e17903ef
Noam Konja <noam.konja@gmail.com>
2023-08-10 12:36:16 +0300

    adding urllib3 library

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from urllib3.util.ssl_ import DEFAULT_CIPHERS
 import json
 
 # Define env parameters / values
